160562fa4630bf3be6873a115ce4568543718d90
patrickmaganga <celestineakinyi2002@gmail.com>
2022-10-15 06:42:33 -0700

    first commit

---
+++ literals.js
@@ -0,0 +1,14 @@
+
+console.log(`i counted ${10+23}goats`);
+let firstName ='celestine';
+let lastName ='akinyi';
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+let totalsOne=500;
+let totalTwo=800;
+let totals= `kshs${totalsOne + totalTwo}`;
+console.log(totals);
+let item = 'cucumbers';
+let price = 1.99;
+let quantity = 4;
+'you bought ${quantity} ${item}, total price: $${price*quantity}';
---
+++ strings.js
@@ -0,0 +1,11 @@
+let annoyingLaugh='teehee so funny! teehee!';
+//annoyingLaugh.replace('teehee','haha');
+console.log(annoyingLaugh); 
+console.log(annoyingLaugh.replace('teehee','haha'));
+const age="5" +"4";
+console.log(age);
+"pecan pie"[7];
+console.log(annoyingLaugh.replaceAll('teehee','haha'));
+//new line
+console.log('hello\'world\nkenya');
+`i counted ${3+4}`;
